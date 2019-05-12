f43e2c198047c56ca0b42d07def05070e7f4b457
Loris Cro <kappaloris@gmail.com>
2019-05-12 23:15:50 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,161 @@
 # zig-cuckoofilter
 Hashing-function Agnostic Cuckoo Filters
+
+**NOTE: this library is still WIP, I haven't yet finalized the interface
+and it's missing useful comptime errors to prevent misuse. 
+It's 100% functionally correct and neat to read though :)**
+
+What's a Cuckoo Filter?
+-----------------------
+Cuckoo filters are a probabilistic data structure that allows you to test for 
+membership of an element in a set without having to hold the whole set in 
+memory.
+
+This is done at the cost of having a probability of getting a false positive 
+response, which, in other words, means that they can only answer "Definitely no" 
+or "Probably yes". The false positive probability is roughly inversely related 
+to how much memory you are willing to allocate to the filter.
+
+The most iconic data structure used for this kind of task are Bloom filters 
+but Cuckoo filters boast both better practical performance and efficiency, and, 
+more importantly, the ability of **deleting elements from the filter**. 
+
+Bloom filters only support insertion of new items.
+Some extensions of Bloom filters have the ability of deleting items but they 
+achieve so at the expense of precision or memory usage, resulting in a far worse 
+tradeoff compared to what Cuckoo filters offer.
+
+
+What Makes This Library Interesting
+----------------------------------------
+Instead of making a Bloom-like interface, I leave to the caller to
+choose a hashing function to use, and which byte(s) of the original item to use
+as fingerprint. This would not give the same ergonomics with Bloom filters as
+they rely on multiple hashings (dozens for low error rates!).
+
+### What are the advantages of doing so?
+	
+- If you are already handling hashed values, no superfluous work is done.
+- To perform well, Cuckoo filters rely on a good choice of fingerprint for each 
+  item and it should not be left to the library.
+- **The hash function can be decided by you, meaning that this module is 
+  hashing-function agnostic**.
+
+The last point is the most important one. 
+It allows you to be more flexible in case you need to reason about item hashes 
+across different clients potentially written in different languages. 
+
+Additionally, different hashing function families specialize on different use 
+cases that might interest you or not. For example some work best for small data 
+(< 7 bytes), some the opposite. Some others focus more on performance at the 
+expense of more collisions, while some others behave better than the rest on 
+peculiar platforms.
+
+[This blogpost](http://aras-p.info/blog/2016/08/09/More-Hash-Function-Tests/) 
+shows a few benchmarks of different hashing function families.
+
+Considering all of that, the choice of hashing func, fingerprinting func should 
+to be up to you. I'm also planning to add a simplified interface, useful for 
+prototiping, so that you don't have to deal with all the details right off the bat.
+
+*For the internal partial hashing that has to happen when reallocating a 
+fingerprint, this implementation uses FNV1a64 which is robust and fast 
+for small inputs (the size of a fingerprint).*
+
+*Thanks to how Cuckoo filters work, that choice is completely transparent to the 
+caller.*
+
+Usage Example
+-------------
+
+```zig
+const std = @import("std");
+const hasher = std.hash.Fnv1a_64;
+const cuckoo = @import("./cuckoofilter.zig");
+
+fn fingerprint8(x: []const u8) u8 {
+    return x[0];
+}
+
+fn print(x: bool) void {
+   std.debug.warn("{}\n", x); 
+}
+
+pub fn main() void {
+    const element = "banana";
+
+    // A cuckoo filter with 1byte long fingerprints (u8)
+    var memory = []u8{0} ** 1024; // Must be a power of 2 
+    var cf8 = cuckoo.Filter8.init(memory[0..]);
+
+
+    const hash = hasher.hash(element);
+    const fp = fingerprint8(element);
+
+    print(cf8.search(hash, fp)); // false
+    cf8.insert(hash, fp) catch unreachable;
+    print(cf8.search(hash, fp)); // true
+    print(cf8.search(hasher.hash("apple"), fingerprint8("apple"))); // false
+    cf8.delete(hash, fp) catch unreachable;
+    print(cf8.search(hash, fp)); // false
+
+
+    // This cuckoo filter uses 4byte long fingerprints (u32) 
+    var new_memory = []u8{0} ** 1024; // Must be a power of 2 
+    var cf32 = cuckoo.Filter32.init(new_memory[0..]);
+    // ...
+}
+```
+
+This will output:
+```
+false
+true
+false
+false
+```
+
+Choosing the right settings
+---------------------------
+TODO
+
+Testing 
+-------
+
+`zig test cucoofilter.zig`
+
+Planned Features
+----------------
+
+- C bindings for the less fortunate that can't use Zig.
+- Add a simplified interface that plucks a fingeprint out of the unused bits of the hash value.
+- Make randomness controllable by the caller, in order to make inserts deterministic.
+- (maybe) Cuckoo filters for multisets: currently you can add a maximum of 
+  `2 * bucketsize` copies of the same element before getting a `FilterError.TooFull` error. 
+  Making a filter that adds a counter for each bucketslot would create a filter 
+  specifically designed for handling multisets. 
+
+License
+-------
+
+MIT License
+
+Copyright (c) 2019 Loris Cro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ cuckoofilter.zig
@@ -0,0 +1,382 @@
+const std = @import("std");
+var   rand = std.rand.DefaultPrng.init(42).random;
+const testing = std.testing;
+
+const FREE_SLOT = 0;
+pub const FilterError = error {
+    BrokenFilter,
+    TooFull,
+};
+
+pub const Filter8 = CuckooFilter(u8, 4);
+pub const Filter16 = CuckooFilter(u16, 4);
+pub const Filter32 = CuckooFilter(u32, 2);
+pub const Filter64 = CuckooFilter(u64, 1);
+
+fn CuckooFilter(comptime FpType: type, comptime buckSize: usize) type {
+    const Bucket = [buckSize]FpType;
+    return struct {
+        homeless_fp: FpType,
+        homeless_bucket_idx: usize,
+        buckets: [] align(1) Bucket,
+
+        const Self = @This();
+        const ScanMode = union(enum) {
+            Set: FpType,
+            Force: FpType,
+            Delete,
+            Search,
+        };
+
+        pub fn init(memory: []u8) Self {
+            return Self {
+                .homeless_fp = FREE_SLOT,
+                .homeless_bucket_idx = undefined,
+                .buckets = @bytesToSlice(Bucket, memory),
+            };
+        }
+
+        pub fn restore(homeless_fp: FpType, homeless_bucket_idx: usize, memory: []u8) Self {
+            return Self {
+                .homeless_fp = homeless_fp,
+                .homeless_bucket_idx = homeless_bucket_idx,
+                .buckets = @bytesToSlice(Bucket, memory),
+            };
+        }
+
+        pub fn search(self: *Self, hash: u64, fingerprint: FpType) bool {
+            const fp = if (FREE_SLOT == fingerprint) 1 else fingerprint;
+            const bucket_idx = hash & (self.buckets.len - 1);
+            // Try primary bucket
+            if (fp == self.scan(bucket_idx, fp, ScanMode.Search)) return true;
+            // Try alt bucket
+            const alt_bucket_idx = self.compute_alt_bucket_idx(bucket_idx, fp);
+            if (fp == self.scan(alt_bucket_idx, fp, ScanMode.Search)) return true
+            else return false;
+        }   
+
+        pub fn delete(self: *Self, hash: u64, fingerprint: FpType) !void {
+            const fp = if (FREE_SLOT == fingerprint) 1 else fingerprint;
+            const bucket_idx = hash & (self.buckets.len - 1);
+            // Try primary bucket
+            if (fp == self.scan(bucket_idx, fp, ScanMode.Delete)) return;
+            // Try alt bucket
+            const alt_bucket_idx = self.compute_alt_bucket_idx(bucket_idx, fp);
+            if (fp == self.scan(alt_bucket_idx, fp, ScanMode.Delete)) return 
+            else return FilterError.BrokenFilter;
+        }
+
+        pub fn insert(self: *Self, hash: u64, fingerprint: FpType) !void {
+            const fp = if (FREE_SLOT == fingerprint) 1 else fingerprint;
+            const bucket_idx = hash & (self.buckets.len - 1);
+            // Try primary bucket
+            if (FREE_SLOT == self.scan(bucket_idx, FREE_SLOT, ScanMode{.Set = fp})) return;
+            // If too tull already, try to add the fp to the secondary slot without forcing
+            if (FREE_SLOT != self.homeless_fp) {
+                if (FREE_SLOT == self.scan(bucket_idx, FREE_SLOT, ScanMode{.Set = fp})) return
+                else return FilterError.TooFull;            
+            }
+            // We are now willing to force the insertion
+            self.homeless_bucket_idx = bucket_idx;
+            self.homeless_fp = fp;
+            var i : usize = 0;
+            while (i < 500) : (i += 1) {
+                self.homeless_bucket_idx = self.compute_alt_bucket_idx(self.homeless_bucket_idx, self.homeless_fp);
+                self.homeless_fp = self.scan(self.homeless_bucket_idx, FREE_SLOT, ScanMode{.Force = self.homeless_fp});
+                if (FREE_SLOT == self.homeless_fp) return;
+            }
+            return FilterError.TooFull;
+        }
+
+        inline fn compute_alt_bucket_idx(self: *Self, bucket_idx: usize, fp: FpType) usize {
+            comptime const fpSize = @sizeOf(FpType);
+            const FNV_OFFSET = 14695981039346656037;
+            const FNV_PRIME = 1099511628211;
+
+            // Note: endianess
+            const bytes = @ptrCast(*const [fpSize] u8, &fp).*;
+            var res: usize = FNV_OFFSET;
+
+            comptime var i = 0;
+            inline while (i < fpSize) : (i += 1) {
+                res ^= bytes[i];
+                res *%= FNV_PRIME;
+            }
+            return (bucket_idx ^ res) & (self.buckets.len - 1);
+        }
+
+        inline fn scan(self: *Self, bucket_idx: u64, fp: FpType, mode: ScanMode) FpType {
+            comptime var i = 0;
+
+            // Search the bucket
+            var bucket = &self.buckets[bucket_idx];
+            inline while (i < buckSize) : (i += 1) {
+                if (bucket[i] == fp) {
+                    switch (mode) {
+                        .Search => {},
+                        .Delete => bucket[i] = FREE_SLOT,
+                        .Set => |val| bucket[i] = val,
+                        .Force => |val| bucket[i] = val,
+                    }
+                    return fp;
+                }
+            }
+
+            switch (mode) {
+                .Search => return FREE_SLOT,
+                .Delete => return FREE_SLOT,
+                .Set => return 1,
+                .Force => |val| {
+                    // We did not find any free slot, so we must now evict.
+                    // TODO: better random approach
+                    const slot = rand.uintLessThanBiased(usize, buckSize);
+                    const evicted = bucket[slot];
+                    bucket[slot] = val;
+                    return evicted;
+                },
+            }
+        }
+
+    };
+}
+
+
+test "Hx == (Hy XOR hash(fp))" {
+    var memory: [1<<20]u8 = undefined;
+    var cf = CuckooFilter(i8, 4).init(memory[0..]);
+    testing.expect(0 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx(0, 'x'), 'x'));
+    testing.expect(1 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx(1, 'x'), 'x'));
+    testing.expect(42 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx(42, 'x'), 'x'));
+    testing.expect(500 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx(500, 'x'), 'x'));
+    testing.expect(5000 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx(5000, 'x'), 'x'));
+    testing.expect(10585 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx(10585, 'x'), 'x'));
+    testing.expect(10586 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx(10586, 'x'), 'x'));
+    testing.expect(18028 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx(18028, 'x'), 'x'));
+    testing.expect((1<<15) - 1 == cf.compute_alt_bucket_idx(cf.compute_alt_bucket_idx((1<<15) - 1, 'x'), 'x'));
+}
+
+fn test_not_broken(cf: var) void {
+    testing.expect(!cf.search(2, 'a'));
+    cf.insert(2, 'a') catch unreachable;
+    testing.expect(cf.search(2, 'a'));
+    testing.expect(!cf.search(0, 'a'));
+    testing.expect(!cf.search(1, 'a'));
+    cf.delete(2, 'a') catch unreachable;
+    testing.expect(!cf.search(2, 'a'));
+}
+
+test "is not completely broken" {
+    var memory = []u8{0} ** 16;
+    var cf = CuckooFilter(u8, 4).init(memory[0..]);
+    test_not_broken(&cf);
+}
+
+
+const Version = struct {
+    FpType: type,
+    buckLen: usize,
+    cftype: type,
+};
+
+const SupportedVersions = []Version {
+    Version { .FpType = u8, .buckLen = 4, .cftype = Filter8},
+    Version { .FpType = u16, .buckLen = 4, .cftype = Filter16},
+    Version { .FpType = u32, .buckLen = 2, .cftype = Filter32},
+    Version { .FpType = u64, .buckLen = 1, .cftype = Filter64},
+};
+
+test "generics are not completely broken" {
+    inline for (SupportedVersions) |v| {
+        var memory = []u8{0} ** 1024;
+        var cf = CuckooFilter(v.FpType, v.buckLen).init(memory[0..]);
+        test_not_broken(&cf);
+    }
+}
+
+test "too full when adding too many copies" {
+    inline for (SupportedVersions) |v| {
+        var memory = []u8{0} ** 1024;
+        var cf = CuckooFilter(v.FpType, v.buckLen).init(memory[0..]);
+        var fp = @intCast(v.FpType, 1);
+        var i: usize = 0;
+        while (i < v.buckLen * 2) : (i += 1) {
+            cf.insert(0, 1) catch unreachable;
+        }
+        testing.expectError(FilterError.TooFull, cf.insert(0, 1));
+        testing.expectError(FilterError.TooFull, cf.insert(0, 1));
+        testing.expectError(FilterError.TooFull, cf.insert(0, 1));
+
+        i = 0;
+        while (i < v.buckLen * 2) : (i += 1) {
+            cf.insert(5, 2) catch unreachable;
+        }
+        testing.expectError(FilterError.TooFull, cf.insert(5, 2));
+        testing.expectError(FilterError.TooFull, cf.insert(5, 2));
+        testing.expectError(FilterError.TooFull, cf.insert(5, 2));
+    }
+}
+
+fn TestSet(comptime FpType: type) type {
+    const ItemSet = std.hash_map.AutoHashMap(u64, FpType);
+    return struct {
+        items: ItemSet,
+        false_positives: ItemSet,
+
+        const Self = @This();
+        fn init(iterations: usize, false_positives: usize, allocator: *std.mem.Allocator) Self {
+            var item_set = ItemSet.init(allocator);
+            var false_set = ItemSet.init(allocator);
+    
+            return Self {
+                .items = blk: {
+                    var i : usize = 0;
+                    while (i < iterations) : (i += 1) {
+                        var hash = rand.int(u64);
+                        while (item_set.contains(hash)) {
+                            hash = rand.int(u64);
+                        }
+                        _ = item_set.put(hash, rand.int(FpType)) catch unreachable;
+                    }
+                    break :blk item_set;
+                },
+
+                .false_positives = blk: {
+                    var i : usize = 0;
+                    while (i < false_positives) : (i += 1) {
+                        var hash = rand.int(u64);
+                        while (item_set.contains(hash) or false_set.contains(hash)) {
+                            hash = rand.int(u64);
+                        }
+                        _ = false_set.put(hash, rand.int(FpType)) catch unreachable;
+                    }
+                    break :blk false_set;
+                },
+            };
+        }
+    };
+}
+
+test "small stress test" {
+    const iterations = 60000;
+    const false_positives = 10000;
+    
+    var direct_allocator = std.heap.DirectAllocator.init();
+    //defer direct_allocator.deinit();
+    inline for (SupportedVersions) |v| {
+        var test_cases = TestSet(v.FpType).init(iterations, false_positives, &direct_allocator.allocator);
+        var iit = test_cases.items.iterator();
+        var fit = test_cases.false_positives.iterator();
+        //defer test_cases.items.deinit();
+        //defer test_cases.false_positives.deinit();
+
+        // Build an appropriately-sized filter
+        const min_size = iterations + @divTrunc(iterations, 4);
+        comptime var twos = std.math.log2(min_size);
+        if ((1 << twos) != min_size) {
+            twos += 1;
+        }
+        var memory = []u8{0} ** ((1 << twos) * @sizeOf(v.FpType));
+        var cf = CuckooFilter(v.FpType, v.buckLen).init(memory[0..]);
+        
+        // Test all items for presence (should all be false)
+        {
+            iit.reset();
+            while (iit.next()) |item| {
+                testing.expect(!cf.search(item.key, item.value));
+            }
+        }
+
+        // Add all items (should not fail)
+        {
+            iit.reset();
+            var iters: usize = 0;
+            while (iit.next()) |item| {
+                cf.insert(item.key, item.value) catch unreachable;
+                iters += 1;
+            }
+        }
+
+        // Test that memory contains the right number of elements
+        {
+            var count: usize = 0;
+            for (@bytesToSlice(v.FpType, memory)) |byte| {
+                if (byte != 0) {
+                    count += 1;
+                }
+            }
+            testing.expect(iterations == count);
+        }
+
+        // Test all items for presence (should all be true)
+        {
+            iit.reset();
+            while (iit.next()) |item| {
+                testing.expect(cf.search(item.key, item.value));
+            }
+        }
+
+        // Delete half the elements and ensure they are not found
+        // (there could be false positives depending on fill lvl)
+        {
+            iit.reset();
+            const max = @divTrunc(iterations, 2);
+            var count: usize = 0;
+            var false_count: usize = 0;
+            while (iit.next()) |item| {
+                count += 1;
+                if (count >= max) break;
+
+                testing.expect(cf.search(item.key, item.value));
+                cf.delete(item.key, item.value) catch unreachable;
+                if(cf.search(item.key, item.value)) false_count += 1;
+            }
+            testing.expect(false_count < @divTrunc(iterations, 40)); // < 2.5%
+
+            iit.reset();
+            count = 0;
+            false_count = 0;
+            while (iit.next()) |item| {
+                count += 1;
+                if (count >= max) break;
+
+                if(cf.search(item.key, item.value)) false_count += 1;
+            }
+            testing.expect(false_count < @divTrunc(iterations, 40)); // < 2.5%
+        }
+
+        // Test false positive elements 
+        {
+            fit.reset();
+            var false_count: usize = 0;
+            while (fit.next()) |item| {
+                if(cf.search(item.key, item.value)) false_count += 1;
+            }
+            testing.expect(false_count < @divTrunc(iterations, 40)); // < 2.5%
+        }
+
+        // Add deleted elements back in and test that all are present
+        {
+            iit.reset();
+            const max = @divTrunc(iterations, 2);
+            var count: usize = 0;
+            var false_count: usize = 0;
+            while (iit.next()) |item| {
+                count += 1;
+                if (count >= max) break;
+
+                cf.insert(item.key, item.value) catch unreachable;
+                testing.expect(cf.search(item.key, item.value));
+            }
+        }
+
+        // Test false positive elements (again)
+        {
+            fit.reset();
+            var false_count: usize = 0;
+            while (fit.next()) |item| {
+                if(cf.search(item.key, item.value)) false_count += 1;
+            }
+            testing.expect(false_count < @divTrunc(iterations, 40)); // < 2.5%
+        }
+    }
+}
